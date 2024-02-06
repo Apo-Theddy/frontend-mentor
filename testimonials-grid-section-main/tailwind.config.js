33da3f787b29a817866cf34ac60e9591646b87dd
Juan jesus <apotheddy>
2024-02-06 01:11:39 -0500

    Testimonials-Grid-Section

---
+++ testimonials-grid-section-main/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "barlow": ["'Barlow Semi Condensed', sans-serif"]
+      },
+      colors: {
+        "moderate-violet": "hsl(263, 55%, 52%)",
+        "very-dark-grayish-blue": "hsl(217, 19%, 35%)",
+        "very-dark-blackhish-blue": "hsl(219, 29%, 14%)",
+        "light-gray": "hsl(0, 0%, 81%)",
+        "light-grayish-blue": "hsl(210, 46%, 95%)",
+      }
+    },
+  },
+  plugins: [],
+}
+
